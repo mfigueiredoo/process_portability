535c5be77b6a644221c03d5cb882493b62d53181
Miguel Figueiredo <94252863+mfigueiredoo@users.noreply.github.com>
2025-02-10 22:19:44 +0000

    - Create a smoother scroll (On card "For Technicians") - Litle change in Css (Color Opacity)

--- scripts/forTechViewport.js
+++ scripts/forTechViewport.js
@@ -1,36 +1,59 @@
-import Scroll from "./scrollAnimation.js"
-
 var ForTechViewport = {
 
 }
 
 //======= EVENTS =======
 
-var touchStartXpos = 0
 var windowRequestaAnimationFrame = undefined
+var touchStartXpos = 0
+var direction = 1
+var distance_per_frame = 0
 var difference = 0
-var speed = 1
+var speed = 0.15
 
 $(".for_technicians-viewport").on("touchstart", (e) => {
     e.preventDefault()
     touchStartXpos = e.targetTouches[0].clientX + $("#for_technicians-wrapper-0001").scrollLeft()
+    direction = 1
+    distance_per_frame = 0
+    difference = 0
 })
 
 $(".for_technicians-viewport").on("touchmove", (e) => {
     e.preventDefault()
-    let target = e.targetTouches[0].target
-    difference = e.targetTouches[0].clientX - touchStartXpos
-
     windowRequestaAnimationFrame = window.requestAnimationFrame(() => {
+        direction = difference > (e.targetTouches[0].clientX - touchStartXpos) ? 1 : -1
+        distance_per_frame = Math.abs(difference - (e.targetTouches[0].clientX - touchStartXpos))
+        difference = e.targetTouches[0].clientX - touchStartXpos
+
         $("#for_technicians-wrapper-0001").animate({
             scrollLeft: -difference
         }, 0);
     })
 })
 
+var touchend_smooth_scroll = () => {
+    distance_per_frame -= speed
+
+    if(distance_per_frame <= 0) {         
+        distance_per_frame = 0   
+        windowRequestaAnimationFrame = undefined
+        return
+    }
+
+    console.log(distance_per_frame);
+
+    $("#for_technicians-wrapper-0001").animate({
+        scrollLeft: $("#for_technicians-wrapper-0001").scrollLeft() + (distance_per_frame * direction)
+    }, 0);
+
+    windowRequestaAnimationFrame = window.requestAnimationFrame(touchend_smooth_scroll)
+
+}
+
 $(".for_technicians-viewport").on("touchend", (e) => {
-    e.preventDefault()
     windowRequestaAnimationFrame = undefined
+    touchend_smooth_scroll()
 })
 
 export default ForTechViewport
